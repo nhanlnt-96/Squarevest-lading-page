094afb485d596f8f95c32c260ad88003dac6fc82
nhanlnt <nhanlnt@smartosc.com>
2022-07-29 23:29:58 +0700

    complete create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         "sq-hauptfarbe-60-percent": "rgba(26, 184, 222, 0.6)",
         "sq-schwarz-für-text": "#333838",
       },
+      boxShadow: {
+        "sq-btn-shadow":
+          "0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)",
+      },
+      padding: {
+        "sq-pb-42-250": "calc(42 / 250 * 100%)", // height / width * 100%
+      },
     },
   },
   plugins: [],
