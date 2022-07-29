b6686242e3db534cc30e81c57f26e2b223aed38a
nhanlnt <nhanlnt@smartosc.com>
2022-07-29 23:04:41 +0700

    add config color and site title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sq-kontrastfarbe": "#246B7D",
+        "sq-hauptfarbe": "#1AB8DE",
+        "sq-hauptfarbe-60-percent": "rgba(26, 184, 222, 0.6)",
+        "sq-schwarz-für-text": "#333838",
+      },
+    },
   },
   plugins: [],
 };
